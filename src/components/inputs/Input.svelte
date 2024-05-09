9ed671472e83bdcbd6f0330bb8e02a29c160f456
pascal Zhang <pascalzhang@MacBook-Pro-de-pascal.local>
2024-05-09 15:23:29 +0200

    add input

---
+++ src/components/inputs/Input.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { inputValue } from '$lib/stores/value.js';
+
+	function onInput(event: EventElements) {
+		inputValue.set(event.currentTarget.value);
+	}
+</script>
+
+<input placeholder="test" on:input={onInput} bind:value={$inputValue} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Input from '$components/inputs/Input.svelte';
+	import { inputValue } from '$lib/stores/value.js';
+</script>
+
+<form>
+	<Input />
+	<p>{$inputValue}</p>
+</form>
