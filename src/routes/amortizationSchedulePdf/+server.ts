9cecdb162751bcbd908caacefa524d0108271146
pascal Zhang <pascalzhang@MacBook-Pro-de-pascal.local>
2024-06-16 15:32:19 +0200

    add error

--- src/routes/amortizationSchedulePdf/+server.ts
+++ src/routes/amortizationSchedulePdf/+server.ts
@@ -1,17 +1,20 @@
 import { generateAmortizationScheduleBuffer } from '$lib/services/pdf';
 
 export async function POST({ request }) {
-	const { immoStore, amortizationScheduleStore } = await request.json();
+	try {
+		const { immoStore, amortizationScheduleStore } = await request.json();
+		const pdfData = await generateAmortizationScheduleBuffer({
+			immoStore,
+			amortizationScheduleStore
+		});
 
-	const pdfData = await generateAmortizationScheduleBuffer({
-		immoStore,
-		amortizationScheduleStore
-	});
-
-	return new Response(pdfData, {
-		headers: {
-			'Content-Type': 'application/pdf',
-			'Content-Disposition': 'inline; filename="amortizationSchedule.pdf"'
-		}
-	});
+		return new Response(pdfData, {
+			headers: {
+				'Content-Type': 'application/pdf',
+				'Content-Disposition': 'inline; filename="amortizationSchedule.pdf"'
+			}
+		});
+	} catch (error) {
+		return new Response(JSON.stringify(error), { status: 500 });
+	}
 }
